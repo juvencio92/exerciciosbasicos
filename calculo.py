481f43ba56ef2c4e347a6bf167fe053a62f41d02
juvencio92 <jorgecamassadila1@gmail.com>
2023-02-01 20:15:28 +0000

    Soma e Multiplicacao de dois números

---
+++ calculo.py
@@ -0,0 +1,8 @@
+num1 = int(input("Introduza o primeiro numero : "))
+num2 = int(input("Introduza o segundo numero : "))
+
+soma = num1 + num2
+multiplicacao = num1 * num2
+
+print("A soma é : ->", soma , " ",
+        " O resultado da multiplicacao é : ->", multiplicacao)
